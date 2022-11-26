1b4bc6cdbdae9de94bab82f46697a8d0b60da4de
gideon <dejiogunbanjo9@gmail.com>
2022-11-26 22:10:21 +0100

    final commit

--- chatbot.py
+++ chatbot.py
@@ -22,9 +22,7 @@ chatbot = ChatBot(
 # Training with Personal Ques & Ans
 training_data_simple = open('training_data/normal.txt').read().splitlines()
 training_data_personal = open('training_data/all.txt').read().splitlines()
-
 training_data = training_data_simple + training_data_personal
-
 trainer = ListTrainer(chatbot)
 trainer.train(training_data)
 
