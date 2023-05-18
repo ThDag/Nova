9a7c53f4ec79a83a4cfe31b6ddd3c3c1efe3fcb1
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-05-19 00:36:08 +0100

    created an empty class and named the bot

---
+++ Alice.py
@@ -0,0 +1,9 @@
+import numpy as np
+# Beginning the Ai
+class ChatBot():
+    def __init__(self, name):
+        print("----- starting up", name, "-----")
+        self.name = name
+# Executing Nova
+if __name__ == "__main__":
+    ai = ChatBot(name="Nova")
