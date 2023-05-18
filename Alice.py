b2c0eeb49a9bfaaec24349c450e92c07d1d0d6c9
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-05-19 00:38:25 +0100

    developed and executed a speech recognition sysytem

--- Alice.py
+++ Alice.py
@@ -7,3 +7,22 @@ class ChatBot():
 # Executing Nova
 if __name__ == "__main__":
     ai = ChatBot(name="Nova")
+    
+# Setting up the Speech recognition section
+import speech_recognition as sr
+def speech_to_text(self):
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as mic:
+         print("listening...")
+         audio = recognizer.listen(mic)
+    try:
+         self.text = recognizer.recognize_google(audio)
+         print("me --> ", self.text)
+    except:
+         print("me -->  ERROR")
+
+# Executing and testing the speech recognition system
+if __name__ == "__main__":
+     ai = ChatBot(name="Dev")
+     while True:
+         ai.speech_to_text()
