9f48a4d10d9b99f4e50a3cad2a1506bcf87c9a38
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-05-19 00:59:10 +0100

    fixed annoying bugs

--- Nova.py
+++ Nova.py
@@ -1,28 +1,35 @@
 import numpy as np
-# Beginning the Ai
+import speech_recognition as sr
+
+
+# Initializing the Ai
+
 class ChatBot():
     def __init__(self, name):
-        print("----- starting up", name, "-----")
+        print("----- starting up", name, "Nova")
         self.name = name
+        
 # Executing Nova
+
 if __name__ == "__main__":
     ai = ChatBot(name="Nova")
     
 # Setting up the Speech recognition section
-import speech_recognition as sr
-def speech_to_text(self):
-    recognizer = sr.Recognizer()
-    with sr.Microphone() as mic:
-         print("listening...")
-         audio = recognizer.listen(mic)
-    try:
-         self.text = recognizer.recognize_google(audio)
-         print("me --> ", self.text)
-    except:
-         print("me -->  ERROR")
+
+    def speech_to_text(self):
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as mic:
+            print("listening...")
+            audio = recognizer.listen(mic)
+        try:
+            self.text = recognizer.recognize_google(audio)
+            print("me --> ", self.text)
+        except:
+            print("me -->  ERROR")
 
 # Executing and testing the speech recognition system
+
 if __name__ == "__main__":
-     ai = ChatBot(name="Dev")
-     while True:
-         ai.speech_to_text()
+    ai = ChatBot(name="Nova")
+    while True:
+        ai.speech_to_text()
