2e739b48aff1a34e7835b794e19db5e28c00e771
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-05-20 01:15:45 +0100

    added functionality for Nova to provide current time

--- Nova.py
+++ Nova.py
@@ -25,10 +25,11 @@ if __name__ == "__main__":
     while True:
         ai.speech_to_text()
 
-# Ensuring that the chatbot will be activated by speaking its name
+# Task 1: Ensuring that the chatbot will be activated by speaking its name
 def wake_up(self, text):
     return True if self.name in text.lower() else False
-# giving Nova the ability to recognize its name
+
+# Task 2: Giving Nova the ability to recognize its name
 from gtts import gTTS
 import os
 @staticmethod
@@ -47,3 +48,29 @@ if __name__ == "__main__":
          if ai.wake_up(ai.text) is True:
              res = "Hello I am Nova your AI, what can I do for you?"
          ai.text_to_speech(res)
+         
+         
+# Task 3: Enabling Nova to provide the current time when asked by the user
+import datetime
+@staticmethod
+def action_time():
+    return datetime.datetime.now().time().strftime('%H:%M')
+#and run the script after adding the above function to the AI class
+# Run the AI
+if __name__ == "__main__":
+    ai = ChatBot(name="Nova")
+    while True:
+            ai.speech_to_text()
+            ## waking up
+            if ai.wake_up(ai.text) is True:
+                res = "Hello I am Nova the AI, what can I do for you?"
+            ## performing any action
+            elif "time" in ai.text:
+                res = ai.action_time()
+            ## responding politely
+            elif any(i in ai.text for i in ["thank","thanks"]):
+                res = np.random.choice(
+                    ["you're welcome!","anytime!",
+                    "no problem!","cool!",
+                    "I'm here if you need me!"])
+            ai.text_to_speech(res)
